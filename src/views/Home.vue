e2797f9689d92282d30b9b0fc7d0d0f04fab6ee4
RV1989 <rien__verbeke@msn.com>
2020-01-14 08:50:16 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-card class="mx-auto" max-width="800">
+      <v-toolbar color="cyan lighten-1" dark>
+        <v-toolbar-title>Sew Drive Tester</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+      </v-toolbar>
+
+      <v-container fluid>
+        <v-form class="pa-4 pt-6">
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field v-model="model" label="opc://"></v-text-field>
+              </v-col>
+              <v-col>
+                <v-file-input multiple label="File input"></v-file-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-spacer></v-spacer>
+              <v-btn big color="primary">Connect</v-btn>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-container>
+
+      <v-container>
+        <v-list>
+          <v-header>Drives</v-header>
+          <v-list-item-group>
+            <v-list-item v-for="(drive, i) in drives" :key="i" :style="{ 'background-color': drive.color }">
+                <v-checkbox v-model="selected"  :value = "drive" color="primary"></v-checkbox>
+              <v-list-item-content>
+                <v-list-item-title v-html="drive.id"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  data() {
+    return {
+      model: "opc://179.158.14:245",
+      selected : [],
+      drives: [
+        { id: "drive1", color: "#80CBC4"},
+        { id: "drive2", color: "#F48FB1" },
+        { id: "drive3", color: "#BBDEFB" },
+        { id: "drive4", color: "#FFFFFF" }
+      ]
+    };
+  },
+
+  components: {}
+};
 </script>
